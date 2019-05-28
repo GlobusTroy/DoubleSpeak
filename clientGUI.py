e4f5254bab332b2f928bec22414ca8070be29b95
GlobusTroy <tem2@illinois.edu>
2019-05-28 00:04:26 -0500

    Refactored clientGUI to use mainly pointers from outside during intiation so the videoClient can direcly update clientGUI data

--- clientGUI.py
+++ clientGUI.py
@@ -6,7 +6,7 @@ from math import sqrt, ceil
 WIDTH=1280
 HEIGHT=720
 
-root = tk.Tk()
+
 # 640x360 vid
 
 GREY = '#666666'
@@ -14,15 +14,13 @@ GREY2 = '#555555'
 BLACK = '#000000'
 GREEN = '#50eda6'
 
-default_avatar = ImageTk.PhotoImage(image=Image.open("avatar_original.jpg"))
-testframe = Image.open("avatar_original.jpg")
-vframe = Image.open("Arstotzka_emblem.png")
+
 
 class GUI:
-    def __init__(self, root, clients=[], height=HEIGHT, width=WIDTH):
-        self.clients = clients
-        self.clientFrames = [None for client in self.clients]
-        self.clientSpeaking = [False for client in self.clients]
+    def __init__(self, root, clientNames={}, currentFrames={}, clientSpeaking={}, height=HEIGHT, width=WIDTH):
+        self.clientNames = clientNames
+        self.currentFrames = currentFrames
+        self.clientSpeaking = clientSpeaking
         self.root = root
         self.root.title('DoubleSpeak')
         style = ttk.Style(root)
@@ -43,14 +41,19 @@ class GUI:
 
         buttonAdd = tk.Button(self.frame, text = "Add Client", command= lambda:self.addClient('new'), 
             highlightbackground = GREY2)
-        buttonAdd.place(relx = 0.1, rely = 0.9, relwidth=0.25, relheight=0.05)
+        #buttonAdd.place(relx = 0.1, rely = 0.9, relwidth=0.25, relheight=0.05)
 
         buttonRm = tk.Button(self.frame, text = "Remove Client", command= lambda:self.removeClient(self.clients[-1]), 
             highlightbackground = GREY2)
-        buttonRm.place(relx = 0.4, rely = 0.9, relwidth=0.25, relheight=0.05)
+        #buttonRm.place(relx = 0.4, rely = 0.9, relwidth=0.25, relheight=0.05)
 
         self.labels = []
-        self.delay = 15
+        self.delay = int(1000/60)
+
+        self.__lastHeight = self.canvas.winfo_height()
+        self.__lastWidth = self.canvas.winfo_width()
+        self.__photoImages = {}
+
         self.root.update()
         self.update()
 
@@ -59,30 +62,37 @@ class GUI:
     def update(self):
         self.canvas.delete('all')
 
-        newLabels = ((len(self.labels) != len(self.clients)) or 
+        #print('GUI Curr Frames: {}'.format(len(self.currentFrames)))
+
+        newLabels = ((len(self.labels) != len(self.currentFrames)) or 
         (self.canvas.winfo_width() != self.__lastWidth) or
         (self.canvas.winfo_height() != self.__lastHeight))
 
         if newLabels:
             for lbl in self.labels:
                 lbl.destroy()
-            self.labels = [None for client in self.clients]
+            self.labels = [None for client in self.currentFrames]
 
         self.__lastHeight = self.canvas.winfo_height()
         self.__lastWidth = self.canvas.winfo_width()
 
-        for i, client in enumerate(self.clients):
-            x,y,w,h = self.getImageDimensions(i, len(self.clients))
-            self.clientFrames[i] = ImageTk.PhotoImage(image = testframe.resize( (w,h), Image.ANTIALIAS ))
-            self.canvas.create_image(x,y, image=self.clientFrames[i], anchor='nw', tag='vid')
+        for i, (clientNum, frame) in enumerate(sorted( self.currentFrames.items() )):
+            x,y,w,h = self.getImageDimensions(i, len(self.currentFrames))
+            
+            #print(frame.shape)
+
+            frameImg = Image.fromarray(frame).resize((w,h), Image.ANTIALIAS)
+            photoImg = ImageTk.PhotoImage(image = frameImg)
+            self.__photoImages[clientNum] = photoImg
+            self.canvas.create_image(x,y, image=photoImg, anchor='nw', tag='vid')
 
             bd = 2
 
             if newLabels:
-                self.labels[i] = tk.Label(self.canvas, text=self.clients[i],fg='white', bg=GREY2)
+                self.labels[i] = tk.Label(self.canvas, text=self.clientNames.get(clientNum, '???'),fg='white', bg=GREY2)
                 self.labels[i].place(x=x+5*bd, y=y+5*bd, anchor='nw')
 
-            ringColor = BLACK if not self.clientSpeaking[i] else GREEN
+            ringColor = BLACK if not self.clientSpeaking.get(clientNum, False) else GREEN
             self.canvas.create_rectangle(x+bd//2,y+bd//2, x+w-bd//2, y+h-bd//2, width=bd, outline=ringColor, tag='rect')
 
         self.root.after(self.delay, self.update)
@@ -124,5 +134,9 @@ class GUI:
         self.clientFrames.pop(idx)
         self.clientSpeaking.pop(idx)
 
-
-GUI(root, clients=['jamie1', 'jamie2'])
+if __name__ == '__main__':
+    root = tk.Tk()
+    default_avatar = ImageTk.PhotoImage(image=Image.open("avatar_original.jpg"))
+    testframe = Image.open("avatar_original.jpg")
+    vframe = Image.open("Arstotzka_emblem.png")
+    GUI(root, clients=['jamie1', 'jamie2'])
